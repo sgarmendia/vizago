b127fb0721c0236a4247e4850f39cdfa0309e90f
Simon Garmendia <sgarmendia@gmail.com>
2017-05-31 01:17:50 +0200

    faceplus test

--- server/routes/vizago/handlers/analyse.js
+++ server/routes/vizago/handlers/analyse.js
@@ -3,7 +3,7 @@ var request = require('request')
 function analyse (req, res) {
 
     // const { url } = req.body
-  console.log('analyse running')
+  
   var options = {
      url: 'https://api-us.faceplusplus.com/facepp/v3/face/analyze?api_key=sMTsq5eWZY-zpF0-yCr5XACBtClsfOvg&api_secret=Br-Og55C1c4lSuuxKDOAqcS9bpLF1SHg&face_tokens=0ed99159cbecb5dc05baea8b30230e29&return_landmark=1&return_attributes=gender,age,smiling,headpose,facequality,blur,eyestatus,ethnicity',
   }
@@ -15,19 +15,25 @@ function analyse (req, res) {
     
   // function callback(error, response, body) {
 
-  //   // if (!error && response.statusCode == 200) {
+  //   if (!error && response.statusCode == 200) {
+
   //     console.log('callback running')
-  //     // var info = JSON.parse(body)
+  //     var info = JSON.parse(body)
   //     console.log(body)
 
-  //   // }
+  //   }
   // }
   
-  request.post(options, function(err,httpResponse,body){
+  request.post(options, function(err,response,body){
 
-   var info = JSON.parse(body)
-  console.log(info)
+    if (!err && response.statusCode == 200) {
+      
+      // console.log('callback running')
+      var info = JSON.parse(body)
+      console.log(info)
 
+    }
+    // console.log(response)
   })
 
   // request(url, callback)
