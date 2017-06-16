0e37a7d3402b715de150e51e3de40dd1b2012423
Simon Garmendia <sgarmendia@gmail.com>
2017-06-16 12:34:36 +0200

    reformat admin for edit

--- src/js/controllers/adminController.js
+++ src/js/controllers/adminController.js
@@ -1,6 +1,37 @@
 function adminController($scope, Upload, vizagoService, ) {
 
-    (function() {
+    (() => {
+
+// FUNCTIONS FOR REGISTER
+        $scope.fileSelected = (files) => {
+            if (files && files.length) {
+                $scope.file = files[0];
+            }
+        }
+
+        $scope.uploadFile = function() {
+
+            const url = '/upload' //node.js route
+            const file = $scope.file
+
+            Upload.upload({ url, file })
+                .success(({ imageLink }) => {
+                    $scope.imageLink = imageLink
+                    console.log(imageLink)
+
+                    const source = imageLink
+                    const { name , auth } = $scope
+
+                    vizagoService.storeUser({ source, name, auth })
+                        .then(storeUserData => {
+
+                            console.log(storeUserData)
+                        })
+
+                })
+        }
+
+//FUNCTIONS FOR EDITING
 
         $scope.fileSelected = (files) => {
             if (files && files.length) {
@@ -29,6 +60,7 @@ function adminController($scope, Upload, vizagoService, ) {
 
                 })
         }
+
 
     })()
 
